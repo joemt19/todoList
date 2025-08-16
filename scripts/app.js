09fa3a65389d8e1d28cf55ed5ddd9103e67b90ba
joemt19 <joembanga19@gmail.com>
2025-08-16 02:13:54 +0100

    Fin de la première partie

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,9 @@
 // Récupération des éléments
+const ajouer =  document.querySelector('form');
+const supprimer = document.querySelector('.delete');
+const champ = document.querySelector('#newtask');
+
+// Ajout des évènements
+ajouer.addEventListener('submit', (e) => {
+    console.log(e);
+})
